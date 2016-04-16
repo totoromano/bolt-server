5feea439fd295002c3d29e6fe83e1b7403d410f3
Claudio Romano <tato121@gmail.com>
2016-04-16 11:59:22 -0400

    changed index to test content

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('Make sure to star the parse-server repo on GitHub! *');
-});
+// app.get('/', function(req, res) {
+//   res.status(200).send('Make sure to star the parse-server repo on GitHub! *');
+// });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
 app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 var port = process.env.PORT || 1337;
