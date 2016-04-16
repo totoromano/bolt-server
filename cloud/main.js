8b2d5716caa70d1459ddceec8f173ab80d7ff6ce
Claudio Romano <tato121@gmail.com>
2016-04-16 17:05:10 -0400

    formatting

--- cloud/main.js
+++ cloud/main.js
@@ -21,9 +21,6 @@ Parse.Cloud.afterSave('transaction',function(req,res){
 
 Parse.Cloud.beforeSave('transaction',function(request,response){
 	 console.log("Transaction saved! - BeforeSave");
-	 console.log(request);
-	request.object.set("amount", 66);
-
 	response.success();
 });
 
