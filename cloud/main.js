b2d513a19474dc5678d991fcebfe1655c852e782
Claudio Romano <tato121@gmail.com>
2016-04-16 14:41:12 -0400

    working on cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,24 +1,39 @@
 
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+  res.success('Hi 2');
 });
 
 //Transaction Methods
 Parse.Cloud.afterSave('transaction',function(req,res){
 	console.log("Transaction saved!");
-	var TransactionObject = Parse.Object.extend("transaction");
-        var transactionObject = new TransactionObject();
-          transactionObject.save({from: "system", "to": "user", "amount": 10}, {
-          success: function(object) {
-            console.log(object);
-          },
-          error: function(model, error) {
-            console.log(error);
-          }
-        });
+	Parse.Cloud.run('pull',{ data: req.params
+	}).then(function(data) {
+              console.log(data);
+     });
+	// Parse.Cloud.httpRequest({
+	//   url: '/transaction/pull',
+	//   body:{
+	//   	req
+	//   }
+	// }).then(function(httpResponse) {
+	//   // success
+	//   console.log(httpResponse.text);
+	// },function(httpResponse) {
+	//   // error
+	//   console.error('Request failed with response code ' + httpResponse.status);
+	// });
+	req.object.set("user_id",1);
+
+	res.sucess();
+
 });
 
 Parse.Cloud.beforeSave('transaction',function(req,res){
 	console.log("Transaction saved!");
 });
 
+
+
+Parse.Cloud.define("pull", function(req,res){
+	res.success(req);
+});
--- index.js
+++ index.js
@@ -39,9 +39,9 @@ app.use(mountPath, api);
 var router = express.Router();   
 router.post('/pull', function(req, res) {
   console.log(req.params);
-    res.json({ message: "sending "+ req.params.data.amount+"- from: "+req.params.data.origin+" to: "+req.params.data.to });   
+  res.json({ message: "sending "+ req.params.data.amount+"- from: "+req.params.data.origin+" to: "+req.params.data.to });   
 });
-app.use('/api', router);
+app.use('/transaction', router);
 
 // Parse Server plays nicely with the rest of your web routes
 // app.get('/', function(req, res) {
