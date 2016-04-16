8bfe7053d80b754f8be604e2e15aeb4af8585b0d
Claudio Romano <tato121@gmail.com>
2016-04-16 14:57:25 -0400

    testing cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -6,15 +6,15 @@ Parse.Cloud.define('hello', function(req, res) {
 //Transaction Methods
 Parse.Cloud.afterSave('transaction',function(req,res){
 	console.log("Transaction saved!");
-	Parse.Cloud.httpRequest({
-	  url: '/transaction/pull'
-	}).then(function(httpResponse) {
-	  // success
-	  console.log(httpResponse.text);
-	},function(httpResponse) {
-	  // error
-	  console.error('Request failed with response code ' + httpResponse.status);
-	});
+	// Parse.Cloud.httpRequest({
+	//   url: '/transaction/pull'
+	// }).then(function(httpResponse) {
+	//   // success
+	//   console.log(httpResponse.text);
+	// },function(httpResponse) {
+	//   // error
+	//   console.error('Request failed with response code ' + httpResponse.status);
+	// });
 	 req.object.set("amount",66);
 
 	res.sucess();
