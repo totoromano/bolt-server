feb2d3086b52a6695c9924386aaef789938c6aaa
Claudio Romano <tato121@gmail.com>
2016-04-16 14:49:50 -0400

    typo fix

--- cloud/main.js
+++ cloud/main.js
@@ -6,19 +6,16 @@ Parse.Cloud.define('hello', function(req, res) {
 //Transaction Methods
 Parse.Cloud.afterSave('transaction',function(req,res){
 	console.log("Transaction saved!");
-	// Parse.Cloud.httpRequest({
-	//   url: '/transaction/pull',
-	//   body:{
-	//   	req
-	//   }
-	// }).then(function(httpResponse) {
-	//   // success
-	//   console.log(httpResponse.text);
-	// },function(httpResponse) {
-	//   // error
-	//   console.error('Request failed with response code ' + httpResponse.status);
-	// });
-	req.object.set("amount",66);
+	Parse.Cloud.httpRequest({
+	  url: '/transaction/pull'
+	}).then(function(httpResponse) {
+	  // success
+	  console.log(httpResponse.text);
+	},function(httpResponse) {
+	  // error
+	  console.error('Request failed with response code ' + httpResponse.status);
+	});
+	// req.object.set("amount",66);
 
 	res.sucess();
 
