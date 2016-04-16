e09feded12f8e6a4f4011656d57d6f82bccb85db
Claudio Romano <tato121@gmail.com>
2016-04-16 14:15:15 -0400

    testing cloud code live

--- cloud/main.js
+++ cloud/main.js
@@ -6,4 +6,19 @@ Parse.Cloud.define('hello', function(req, res) {
 //Transaction Methods
 Parse.Cloud.afterSave('transaction',function(req,res){
 	console.log("Transaction saved!");
+	var TransactionObject = Parse.Object.extend("transaction");
+        var transactionObject = new TransactionObject();
+          transactionObject.save({from: "system", "to": "user", "amount": 10}, {
+          success: function(object) {
+            console.log(object);
+          },
+          error: function(model, error) {
+            console.log(error);
+          }
+        });
 });
+
+Parse.Cloud.beforeSave('transaction',function(req,res){
+	console.log("Transaction saved!");
+});
+
