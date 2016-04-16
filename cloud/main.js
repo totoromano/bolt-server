5c0a74c5680fe5866bfbd3ad9a9783ea2eb3a7b9
Claudio Romano <tato121@gmail.com>
2016-04-16 17:24:25 -0400

    push method

--- cloud/main.js
+++ cloud/main.js
@@ -95,9 +95,74 @@ Parse.Cloud.define("pull", function(req,res){
 			//       alert("Error: " + error.code + " " + error.message);
 			//    }
 			// });
+			Parse.Cloud.run('push', { id: "3"}).then(function(data) {
+			      console.log(data);
+			  },function(error){
+			  	console.log(error);
+			  });
 	   } else {
 	     console.log("Got error: " + error.message);
 	   }
 	 }
 	);
 });
+Parse.Cloud.define("push", function(req,res){
+	var request = require('request');
+	var req = request.defaults();
+	var fs = require('fs');
+
+	var data = JSON.stringify({
+	  "systemsTraceAuditNumber":"451000",
+	  "retrievalReferenceNumber":"330000550000",
+	  "localTransactionDateTime":"2016-04-16T18:56:11",
+	  "acquiringBin":"408999",
+	  "acquirerCountryCode":"840",
+	  "senderAccountNumber":"4957030420210454",
+	  "recipientPrimaryAccountNumber":"4957030420210462",
+	  "cardAcceptor": {
+	    "address":{
+	      "country":"USA",
+	      "state":"CA",
+	      "zipCode":"33196"
+	    },
+	    "name":"Acceptor 1",
+	    "terminalId":"365539",
+	    "idCode":"VMT200911026070"
+	  },
+	  "amount":"350",
+	  "transactionCurrencyCode": "USD",
+	  "businessApplicationId":"AA",
+	  "recipientName": "rohan"
+	});
+
+	var userId = '1JU5YLFNJP0ISHOWB4RL21rjbOfWnkbbE09d06EKCMQcIVcQs';
+	var password = 'BBlUAZ1kcNEYZFB738K6Z';
+	var keyFile = 'certificates/key_Bolt.pem';
+	var certificateFile ='certificates/cert.pem';
+
+	req.post({
+	    uri : "https://sandbox.api.visa.com/visadirect/fundstransfer/v1/pushfundstransactions",
+	    key: fs.readFileSync(keyFile),
+	    cert: fs.readFileSync(certificateFile),
+	    headers: {
+	      'Content-Type' : 'application/json',
+	      'Accept' : 'application/json',
+	      'Authorization' : 'Basic ' + new Buffer(userId + ':' + password).toString('base64')
+	    },
+	    body: data
+	  }, function(error, response, body) {
+	    if (!error) {
+	      console.log("Response Code: " + response.statusCode);
+	      console.log("Headers:");
+	      for(var item in response.headers) {
+	        console.log(item + ": " + response.headers[item]);
+	      }
+	      console.log("Body: "+ body);
+	    } else {
+	      console.log("Got error: " + error.message);
+	    }
+	  }
+	);
+	
+});
+
