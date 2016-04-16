154af39eedaa11b8979c1c13f63f4d0b7be6ff45
Claudio Romano <tato121@gmail.com>
2016-04-16 15:25:57 -0400

    logging after save hit api

--- cloud/main.js
+++ cloud/main.js
@@ -6,27 +6,15 @@ Parse.Cloud.define('hello', function(req, res) {
 //Transaction Methods
 Parse.Cloud.afterSave('transaction',function(req,res){
 	console.log("Transaction saved! - AfterSave");
-	// Parse.Cloud.httpRequest({
-	//   url: '/transaction/pull'
-	// }).then(function(httpResponse) {
-	//   // success
-	//   console.log(httpResponse.text);
-	// },function(httpResponse) {
-	//   // error
-	//   console.error('Request failed with response code ' + httpResponse.status);
-	// });
-	// query = new Parse.Query("transaction");
- //  query.get(request.object.get("transaction").id, {
- //    success: function(operation) {
- //      operation.set("amount",99);
- //      operation.save();
- //    },
- //    error: function(error) {
- //      console.error("Got an error " + error.code + " : " + error.message);
- //    }
- //  });
-	 
-
+	Parse.Cloud.httpRequest({
+	  url: '/transaction/pull'
+	}).then(function(data) {
+	  // success
+	  console.log(data);
+	},function(error) {
+	  // error
+	  console.error('Request failed with response code ' + error.status);
+	});
 });
 
 Parse.Cloud.beforeSave('transaction',function(request,response){
