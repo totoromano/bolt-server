8833df8c8a24efee74f13499cacc749f6e07a617
Claudio Romano <tato121@gmail.com>
2016-04-16 21:35:20 -0400

    afterSave work on bolt task zip code

--- cloud/main.js
+++ cloud/main.js
@@ -225,7 +225,7 @@ Parse.Cloud.define("setBalance", function(req,res){
 
 
 //Bolt Task Methods
-Parse.Cloud.beforeSave('boltTask',function(request,response){
+Parse.Cloud.afterSave('boltTask',function(request,response){
 	console.log("Task at: "+request.object.get("zip"));
 	Parse.Cloud.httpRequest({
 	  url: 'http://maps.googleapis.com/maps/api/geocode/json?',
